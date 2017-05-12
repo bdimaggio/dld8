e7fc09331535ffeceb63bfe63317164e1c6ba1d9
Ben Di Maggio <ben@digital-loom.com>
2017-05-12 17:56:45 -0400

    Adding Pantheon core files

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite5698b097380164c98a1b609423ac845::getLoader();
+return ComposerAutoloaderInitDrupal8::getLoader();
